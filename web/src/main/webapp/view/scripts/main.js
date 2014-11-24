60874e6674fd312231d466079c6707dc74fa1997
Eliseev Alexandr <eliseealex@gmail.com>
2014-11-24 23:22:26 +0300

    #20 game addition

--- web/src/main/webapp/view/scripts/main.js
+++ web/src/main/webapp/view/scripts/main.js
@@ -16,6 +16,11 @@ app.config(function ($routeProvider) {
         .when('/word/:word/:language', {
             templateUrl: 'pages/word.html',
             controller: 'Word'
+        })
+
+        .when('/game', {
+            templateUrl: 'pages/answersGame.html',
+            controller: 'Game'
         });
 });
 
@@ -40,7 +45,7 @@ app.factory('LanguageFactory', function LanguageFactory($http, $rootScope) {
     LanguageFactory.refreshLanguages();
 
     return {
-        getLanguages: function() {
+        getLanguages: function () {
             return LanguageFactory.languages;
         },
         refreshLanguages: LanguageFactory.refreshLanguages
@@ -65,11 +70,79 @@ app.factory('UserLanguageFactory', function UserLanguageFactory($http, $rootScop
     }
 
     return {
-        getLanguage: function() {
+        getLanguage: function () {
             return language;
         },
         refreshLanguage: refreshLanguage
     }
 });
 
+angular.module('main').controller('Game', function ($scope) {
+    $scope.question = 'Find a good translation';
+
+    $scope.quests = [{
+        word: 'Word', options: [{word: 'Wort'}, {word: 'Morgen'}, {word: 'Platz'}, {word: 'Spiele'}],
+        answer: 0
+    }, {
+        word: 'Auge', options: [{word: 'Magic'}, {word: 'Morning'}, {word: 'Sun'}, {word: 'Eye'}],
+        answer: 3
+    }];
+
+    $scope.round = 0;
+    $scope.answered = false;
+    $scope.progress = 0;
+
+    $scope.setState = function(i) {
+        var word = $scope.quests[i];
+        $scope.word = word.word;
+
+        function extract(n) {
+            return word.options[n].word;
+        }
+
+        $scope.first = extract(0);
+        $scope.second = extract(1);
+        $scope.third = extract(2);
+        $scope.fourth = extract(3);
+        $scope.writeAnswer = word.answer;
+
+        $scope.progress = (i/$scope.quests.length)*100
+    };
+
+    $scope.style = function(i) {
+        if($scope.answered) {
+            if (i == $scope.writeAnswer+1) {
+                return "btn-success";
+            }
+            else if (i == $scope.choosen) {
+                return "btn-danger";
+            }
+            else {
+                return "btn-default";
+            }
+        } else {
+            return "btn-default";
+        }
+    };
+
+    $scope.choose = function(i) {
+        if (!$scope.answered) {
+            $scope.answered = true;
+            $scope.choosen = i;
+        }
+    };
+
+    $scope.isAnswered = function () {
+        return $scope.answered;
+    };
+
+    $scope.next = function () {
+        $scope.answered = false;
+        $scope.round += 1;
+        $scope.setState($scope.round);
+    };
+
+
+    $scope.setState($scope.round);
+});
 // TODO add factory with user factory
